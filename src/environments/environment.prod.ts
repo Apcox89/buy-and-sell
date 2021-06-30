90aee75ce155645e002526c1afa8bfd259ec2edf
Apcox89 <apcox89@gmail.com>
2021-06-30 09:21:06 -0500

    init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAZiAX-nWKUBLqbo9DXYzu1PdebFElQ_L0",
+    authDomain: "buy-and-sell-f7cab.firebaseapp.com",
+    projectId: "buy-and-sell-f7cab",
+    storageBucket: "buy-and-sell-f7cab.appspot.com",
+    messagingSenderId: "951799223758",
+    appId: "1:951799223758:web:92d9e74f0a15184091879f",
+    measurementId: "G-48KGZ10768"    
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAZiAX-nWKUBLqbo9DXYzu1PdebFElQ_L0",
+    authDomain: "buy-and-sell-f7cab.firebaseapp.com",
+    projectId: "buy-and-sell-f7cab",
+    storageBucket: "buy-and-sell-f7cab.appspot.com",
+    messagingSenderId: "951799223758",
+    appId: "1:951799223758:web:92d9e74f0a15184091879f",
+    measurementId: "G-48KGZ10768"    
+  }
 };
 
 /*
